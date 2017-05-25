56261e67b54a9c45f0c79f94ff4c08c0292e53e2
steveoh <sgourley@utah.gov>
2017-05-24 18:01:48 -0600

    fix slashes
    r are dangerous

--- secrets.sample.py
+++ secrets.sample.py
@@ -1,3 +1,3 @@
 OPEN_QUAD_WORD = 'your-open-quad-word'
 # this path is split up to prevent AGS from trying to copy this folder to the server when publishing
-AGS_OUTPUT_DIR = r'C:\\' + r'arcgisserver\directories\arcgisoutput'
+AGS_OUTPUT_DIR = 'C:\\' + 'arcgisserver\\directories\\arcgisoutput'
